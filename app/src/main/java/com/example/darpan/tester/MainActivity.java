03cd83d7e17141a00c1afb22ab66937ba5c06bf7
ZainRasheed <rasheedulla97@gmail.com>
2018-09-29 11:47:24 +0530

    Intents

--- app/src/main/java/com/example/darpan/tester/MainActivity.java
+++ app/src/main/java/com/example/darpan/tester/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.darpan.tester;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void register(View view) {
+        Intent i= new Intent(this, Main2Activity.class);
+        startActivity(i);
+    }
+
+    public void login(View view) {
+        Intent i= new Intent(this, Main3Activity.class);
+        startActivity(i);
     }
 }
